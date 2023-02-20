0c480796bbea0f3cde12004665b0260a4ba3be2d
Bradley Wayne Arnold <82877822+B1r2a3d4l5y@users.noreply.github.com>
2023-02-20 14:18:43 +0200

    Add files via upload

---
+++ js/hidden_text.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $("#welcome").click(function () {
+        $("#about").show().animate({opacity:1})
+     })
+     $("#welcome").click(function(){
+        $("#description").show().animate({opacity:1})
+     })
+
+})
