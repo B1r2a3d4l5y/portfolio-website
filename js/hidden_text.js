cfc6a543d110675d0b091b2563b1e327453a86c5
Bradley Wayne Arnold <82877822+B1r2a3d4l5y@users.noreply.github.com>
2023-02-27 15:16:37 +0200

    Add files via upload

--- js/hidden_text.js
+++ js/hidden_text.js
@@ -4,6 +4,19 @@ $(document).ready(function(){
      })
      $("#welcome").click(function(){
         $("#description").show().animate({opacity:1})
+     });
+     $("#welcome").click(function(){
+      $("#projects").show().animate({opacity:1});
+
+     });
+     //project links
+     $("#welcome").click(function(){
+      $("#project-links-netflix").show().animate({opacity:1})
+     });
+     $("welcome").click(function(){
+      //disney+ project link
+      $("#project-links-disney").show().animate({opacity:1})
+
      })
 
-})
+     })
